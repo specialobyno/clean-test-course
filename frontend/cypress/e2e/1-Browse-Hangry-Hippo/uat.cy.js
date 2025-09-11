059cfc5517f84afc9fcc23dd16de04ce2aacceff
specialobyno <specialobyno@yahoo.com>
2025-09-11 03:42:13 +0100

    added cypress test

--- frontend/cypress/e2e/1-Browse-Hangry-Hippo/uat.cy.js
+++ frontend/cypress/e2e/1-Browse-Hangry-Hippo/uat.cy.js
@@ -1,11 +1,19 @@
-describe('Shows Appeteasers', () => {
-  it('Connect to Dev Server', () => {
-    cy.visit('https://hangryhippo.quantic.host/');
+describe("Shows Appeteasers", () => {
+  it("Connect to Dev Server", () => {
+    cy.visit("https://hangryhippo.quantic.host/");
   });
-  it('selects Handhelds', () => {
+  it("selects Handhelds", () => {
+    cy.contains("Handhelds").click();
+    cy.contains("Cheese Burger");
+    cy.contains("Fajita Tacos");
     //TODO: Add a test to check Handhelds
   });
-  it('selects Appeteasers', () => {
+  it("selects Appeteasers", () => {
+    cy.contains("Appeteasers").click();
+    cy.contains("Tater Tots");
+    cy.contains("Buffalo Wings");
+    cy.contains("Cheese Burger").should("not.exist");
+    cy.contains("Fajita Tacos").should("not.exist");
     //TODO: Add a test to check Appeteasers
   });
 });
